f0107701267fbb79347f5ba7f7754d9eba702818
Rafael Alejandro Domínguez Junco <yayo281001@hotmail.com>
2024-03-06 10:06:33 -0600

    06/03

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -1,12 +1,51 @@
-import { Component } from "@angular/core";
+// Así se hace de forma manual
 
+// import { Component } from "@angular/core";
+
+
+// @Component({
+//   selector: 'app-counter',
+//   template: '<h1>Hola Counter</h1>',
+// })
+
+
+// export class CounterComponent{
+
+// }
+
+// Se puede usar el snippet "a-component"
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>',
+  template: `
+
+  <h3>
+    Counter: {{ counter }}
+  </h3>
+
+  <button (click)="increaseBy(1)"> + </button>
+  <button (click)="resetCounter()"> Reset </button>
+  <button (click)="increaseBy(-1)"> - </button>
+
+
+  `
 })
 
+export class CounterComponent {
 
-export class CounterComponent{
+  public counter: number = 10;
+
+  public increaseBy( value: number): void{
+    this.counter += 1
+
+  }
+
+  public resetCounter(){
+    this.counter = 10;
+  }
+
+  constructor() { }
 
 }
